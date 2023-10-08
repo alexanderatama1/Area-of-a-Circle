fa056df375f1b9ad4692c305342dea465c89cedb
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-10-08 17:35:51 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Declare the variables
+double radius, area;
+
+// Prompt for Radius
+Console.Write("Enter a radius ");
+radius = double.Parse (Console.ReadLine());
+
+// Prompt for area(of a circle)
+area = Math.PI * Math.Pow(radius, 2);
+
+// Output to the console
+Console.WriteLine("A circle with radius " + radius + " has an area of " + area + ".");
